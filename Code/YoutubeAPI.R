6e0a9e619bd0d42c638057f427db9f27354a72b7
joRauser <jonas.rauser@web.de>
2025-06-05 08:12:49 +0200

    Paused the code to get followers because its using way to much quota from YoutubeAPI Created Sheet: dataAnalysis

--- Code/YoutubeAPI.R
+++ Code/YoutubeAPI.R
@@ -50,7 +50,36 @@ res_df <- bind_rows(lapply(res, as.data.frame))
 
 library(stringr)
 
-# TODO: Get videoname and uploaddate with "get_video_details" 
+# needed to get the subscribers of the artists => ON PAUSE
+get_max_subscribers <- function(artist_name) {
+  # Fehler abfangen & Ausgabe unterdrücken
+  suppressMessages(
+    tryCatch({
+      channel_ids <- yt_search(artist_name, type = "channel") %>%
+        head(5) %>%
+        pull(channelId)
+      
+      if (length(channel_ids) == 0) return(NA_real_)
+      
+      sub_counts <- numeric(length(channel_ids))
+      
+      for (i in seq_along(channel_ids)) {
+        chnStats <- tryCatch(get_channel_stats(channel_ids[i]), error = function(e) NULL)
+        
+        sub_counts[i] <- if (!is.null(chnStats$statistics$subscriberCount)) {
+          as.numeric(chnStats$statistics$subscriberCount)
+        } else {
+          NA_real_
+        }
+      }
+      
+      max(sub_counts, na.rm = TRUE)
+    }, error = function(e) NA_real_)
+  )
+}
+
+
+
 res_df <- res_df %>%
   rowwise() %>%
   mutate(
@@ -75,15 +104,18 @@ res_df <- res_df %>%
   arrange(artist, publishedAt) %>%
   group_by(artist) %>%
   mutate(concertNumber = row_number()) %>%
-  ungroup()
+  ungroup() 
+  # Get number of Followers per Artist => On Pause bcs of overload on API
+  # mutate(artistFollowers = map_dbl(as.character(artist), get_max_subscribers)) 
+
 
 
 
 # ToDo: 
-# - Nach Künstler-Followern suchen 
+# - Nach Künstler-Followern suchen -> Später überprüfen ob funktioniert (Kontingent verbraucht)
 # -> Use yt_search for channels. Search multiple channels (first five or sth. and return the one with max followers)
-# - Spalte mit "How many Concerts" einführen
+
 # - Code auf gesamten Datensatz anwenden
-
-
-
+# - Datensatz säubern (zB Datentyp bei den Datums)
+# - Anwendung wie bei handData
+  
